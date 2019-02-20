25fe1768298eed4f6c8f5f02eee9f36077845ad7
James W Moody <jamesmoodyradio@gmail.com>
2019-02-20 12:19:45 -0800

    Add unit tests

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 'use strict';
 
 const boom = require('boom');
+const async = require('async');
 const csvString = require('csv-string');
 
 exports.filterCsv = function (csv, filterOpts, callback) {
     const csvArray = csvString.parse(csv);
     const firstRow = csvArray[0];
-    const filteredCsv = [];
+    let filteredCsv = [];
     let filterColumnIndex;
   
     if (filterOpts.hasHeader) {
       filteredCsv.push(firstRow);
     }
-  
+
     if (typeof filterOpts.columnToFilter === 'string' && !parseInt(filterOpts.columnToFilter)) {
       filterColumnIndex = firstRow.indexOf(filterOpts.columnToFilter);
-      csvArray.forEach(function(row) {
-        if (row[filterColumnIndex] === filterOpts.filterCriteria) {
-          filteredCsv.push(row);
-        }
-      });
     } else {
       filterColumnIndex = parseInt(filterOpts.columnToFilter) - 1;
-      csvArray.forEach(function(row) {
-        if (row[filterColumnIndex] === filterOpts.filterCriteria) {
-          filteredCsv.push(row);
-        }
-      });
     }
-    return callback(null, filteredCsv);
-}
 
+    async.each(csvArray, function(row, callback) {
+      if (row[filterColumnIndex] === filterOpts.filterCriteria) {
+        filteredCsv.push(row);
+      }
+      callback();
+    }, function(err) {
+      filteredCsv = filteredCsv
+        .join('\n');
+      return callback(null, filteredCsv);
+    });
+}
 
 // TODO: Write sort function
 // exports.sortCsv = function (csv, sortOpts, callback) {
